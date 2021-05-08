a651df43630ea2b3d84ca392792a8d3976e19466
EndOfMaster <843698826@qq.com>
2021-05-08 20:43:52 +0800

    start

---
+++ contracts/WkSwapRouter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract WkSwapRouter is Ownable{
+
+    using SafeMath for uint256;
+
+
+
+}
